eaecccac58c79c6a5e421434f4167d6e5cc9716d
skessler01 <108894472+skessler01@users.noreply.github.com>
2022-10-10 14:18:36 +0200

    Add Operation3

--- Branches/python_template.py
+++ Branches/python_template.py
@@ -10,9 +10,15 @@ def operation(a, b, c, d):
 
 def operation2(a,b):
     return a-b
+    
+def operation3(a,b):
+    return a*b
+
 # Execute main operation
 if __name__ == "__main__":
     op_result = operation(varA, varB, varC, varD)
     op2_result = operation2(varA,varB)
+    op3_result = operation3(varA,varB)
     print("Result value: ", op_result)
     print("Result value2: ", op2_result)
+    print("Result value3: ", op3_result)
