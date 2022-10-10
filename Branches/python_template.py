177b9914998fcf2a80a61807eab4a4dfb513a0b4
Moritz Flüchter <moritz.fluechter@student.uni-tuebingen.de>
2022-10-10 14:15:34 +0200

    Revert "Branch1"
    This reverts commit 72ad571738c236cd2fe482b58d94f56e24343568.

--- Branches/python_template.py
+++ Branches/python_template.py
@@ -8,11 +8,7 @@ def operation(a, b, c, d):
     result = a+b+c+d
     return result
 
-def operation2(a,b):
-    return a-b
 # Execute main operation
 if __name__ == "__main__":
     op_result = operation(varA, varB, varC, varD)
-    op2_result = operation2(varA,varB)
     print("Result value: ", op_result)
-    print("Result value2: ", op2_result)
